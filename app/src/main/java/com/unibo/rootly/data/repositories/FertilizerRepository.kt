95f4efa6bcaa0d02086b27ee94e10288730a845e
Coli Alessandro <alessandro.coli2@studio.unibo.it>
2024-05-08 16:02:43 +0200

    fix(repositories): added missing import

--- app/src/main/java/com/unibo/rootly/data/repositories/FertilizerRepository.kt
+++ app/src/main/java/com/unibo/rootly/data/repositories/FertilizerRepository.kt
@@ -1,6 +1,7 @@
 package com.unibo.rootly.data.repositories
 
 import com.unibo.rootly.data.database.Fertilizer
+import com.unibo.rootly.data.database.daos.FertilizerDao
 
 class FertilizerRepository(
     private val fertilizerDao: FertilizerDao
