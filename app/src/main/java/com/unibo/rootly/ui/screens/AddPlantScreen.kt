140d9254ec198437fb70d2c22e1228beb712b761
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-05-09 16:58:51 +0200

    dev(ui): impl of add-plant screen

--- app/src/main/java/com/unibo/rootly/ui/screens/AddPlantScreen.kt
+++ app/src/main/java/com/unibo/rootly/ui/screens/AddPlantScreen.kt
@@ -1,15 +1,28 @@
 package com.unibo.rootly.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Image
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.unibo.rootly.ui.RootlyRoute
 import com.unibo.rootly.ui.composables.TopBar
@@ -18,17 +31,72 @@ import com.unibo.rootly.ui.composables.TopBar
 fun AddPlantScreen(
     navController: NavHostController
 ) {
-   Scaffold(
-       topBar = { TopBar(navController = navController, currentRoute = RootlyRoute.AddPlant) }
-   ) {contentPadding ->
-       Box(
-           contentAlignment = Alignment.Center,
-           modifier = Modifier
-               .fillMaxSize()
-               .padding(contentPadding)
-               .fillMaxWidth()
-       ) {
-           Text("Add Plant Screen")
-       }
-   }
+    var name by rememberSaveable { mutableStateOf("") }
+    var type by rememberSaveable { mutableStateOf("") }
+    var waterDate by rememberSaveable { mutableStateOf("") }
+    var fertilizerDate by rememberSaveable { mutableStateOf("") }
+
+    Scaffold(
+        topBar = { TopBar(
+            navController = navController,
+            currentRoute = RootlyRoute.AddPlant
+            )
+        }
+    ) { contentPadding ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(6.dp),
+            modifier = Modifier
+                .padding(contentPadding)
+                .padding(36.dp)
+                .fillMaxWidth()
+        ) {
+            Image(
+                Icons.Outlined.Image,
+                contentDescription = "Plant photo",
+                modifier = Modifier
+                    .clip(RoundedCornerShape(28.dp))
+                    .background(MaterialTheme.colorScheme.secondaryContainer)
+                    .padding(128.dp)
+                    .fillMaxWidth()
+            )
+
+            // Data input fields
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = name,
+                onValueChange = { name = it },
+                label = { Text("Name") }
+            )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = type,
+                onValueChange = { type = it },
+                label = { Text("Type") }
+            )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = waterDate,
+                onValueChange = { waterDate = it },
+                label = { Text("Date (Water)") },
+                placeholder = { Text("dd/mm/yyyy") }
+            )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = fertilizerDate,
+                onValueChange = { fertilizerDate = it },
+                label = { Text("Date (Fertilizer)") },
+                placeholder = { Text("dd/mm/yyyy") }
+            )
+
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp)
+            ) {
+                Text("Add")
+            }
+        }
+    }
 }
