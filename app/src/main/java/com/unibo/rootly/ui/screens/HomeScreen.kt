aac7c7b6f000c74718c1ec4f6fc7b7a1b437bca6
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-05-09 09:59:35 +0200

    dev(ui): improved ui homepage, now displaying items

--- app/src/main/java/com/unibo/rootly/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/unibo/rootly/ui/screens/HomeScreen.kt
@@ -1,29 +1,53 @@
 package com.unibo.rootly.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.unibo.rootly.ui.RootlyRoute
 import com.unibo.rootly.ui.composables.BottomBar
 
 @Composable
-fun HomeScreen(
-    navController: NavHostController
-) {
+fun HomeScreen(navController: NavHostController) {
+    val todayPlants = (1..2).map { "Plant n°$it" } //TODO: add real plants from db
+    val soonPlants = (3..20).map { "Plant n°$it" }
+
     Scaffold (
         floatingActionButton = {
             FloatingActionButton(
@@ -40,17 +64,101 @@ fun HomeScreen(
             )
         }
     ) { contentPadding ->
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.secondaryContainer)
-                .padding(contentPadding)
-                .fillMaxWidth()
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(1),
+            verticalArrangement = Arrangement.spacedBy(15.dp),
+            contentPadding = PaddingValues(8.dp, 8.dp),
+            modifier = Modifier.padding(contentPadding)
         ) {
-            Text("Home")
+            item {
+                Row(
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    Text(
+                        text = RootlyRoute.Home.title,
+                        style = TextStyle(
+                            fontFamily = FontFamily.Serif,
+                            fontSize = 50.sp,
+                            fontWeight = FontWeight.Bold,
+                        ),
+                        modifier = Modifier.weight(1F)
+                    )
+                    OutlinedButton(
+                        onClick = { /* TODO */ },
+                        modifier = Modifier.weight(1F)
+                    ) {
+                        Icon(Icons.Outlined.Add, "Add filter")
+                        Text("Add Filters")
+                    }
+                }
+            }
+            items(todayPlants) { item ->
+                TravelItem(
+                    item,
+                    onClick = {
+                        //navController.navigate(RootlyRoute.PlantDetails.buildRoute(item)) TODO
+                    }
+                )
+            }
+            item {
+                Text(
+                    text = "Soon",
+                    style = TextStyle(
+                        fontFamily = FontFamily.Serif,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                )
+            }
+            items(soonPlants) { item ->
+                TravelItem(
+                    item,
+                    onClick = {
+                        //navController.navigate(RootlyRoute.PlantDetails.buildRoute(item)) TODO
+                    }
+                )
+            }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TravelItem(item: String, onClick: () -> Unit) {
+    Card(
+        onClick = onClick,
+        modifier = Modifier
+            .size(150.dp)
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.secondaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                Icons.Outlined.Search,
+                "Travel picture",
+                contentScale = ContentScale.Fit,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSecondary),
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.secondary)
+                    .padding(20.dp)
+            )
+            Spacer(Modifier.size(8.dp))
+            Text(
+                item,
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
