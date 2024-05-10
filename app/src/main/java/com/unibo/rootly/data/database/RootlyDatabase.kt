9a549248db4380a0dcca9a9129a041307773ad7c
Coli Alessandro <alessandro.coli2@studio.unibo.it>
2024-05-10 18:39:49 +0200

    fix(db): fixed date insert

--- app/src/main/java/com/unibo/rootly/data/database/RootlyDatabase.kt
+++ app/src/main/java/com/unibo/rootly/data/database/RootlyDatabase.kt
@@ -82,13 +82,13 @@ abstract class RootlyDatabase : RoomDatabase() {
 object Converters {
     @TypeConverter
     @JvmStatic
-    fun fromTimestamp(value: Long?): LocalDate? {
-        return value?.let { LocalDate.ofEpochDay(it) }
+    fun fromTimestamp(value: String?): LocalDate? {
+        return value?.let { LocalDate.parse(it) }
     }
 
     @TypeConverter
     @JvmStatic
-    fun dateToTimestamp(date: LocalDate?): Long? {
-        return date?.toEpochDay()
+    fun dateToTimestamp(date: LocalDate?): String? {
+        return date?.toString()
     }
 }
