69623c6816ec707d62221b1cac39f506b37ecdeb
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-05-13 10:37:19 +0200

    feat(image): added a composable to display images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,4 +86,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
     implementation("io.coil-kt:coil-compose:2.3.0")
+    implementation("androidx.activity:activity-ktx:1.6.1")
+    implementation("androidx.activity:activity-compose:1.6.1")
+    implementation("io.coil-kt:coil-compose:2.2.2")
 }
