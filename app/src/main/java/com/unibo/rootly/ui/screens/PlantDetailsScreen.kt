931c2f6eae06229637b4e789ba115bb02189727a
Coli Alessandro <alessandro.coli2@studio.unibo.it>
2024-06-04 16:41:19 +0200

    fix(images): better img view

--- app/src/main/java/com/unibo/rootly/ui/screens/PlantDetailsScreen.kt
+++ app/src/main/java/com/unibo/rootly/ui/screens/PlantDetailsScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -34,6 +35,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -104,7 +106,9 @@ fun PlantDetailsScreen(
                 contentDescription = "Plant photo",
                 modifier = Modifier
                     .clip(RoundedCornerShape(28.dp))
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .heightIn(min = 256.dp,  max = 435.dp),
+                contentScale = ContentScale.Fit,
                 defaultHigh = 256.dp
             )
             FilledTonalIconButton(
